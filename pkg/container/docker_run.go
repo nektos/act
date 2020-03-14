3baf626aa417c64a92a4af9e79209c4cbe130e31
Casey Lee <cplee@nektos.com>
2020-03-13 23:21:25 -0700

    add file info to files cp into container

--- pkg/container/docker_run.go
+++ pkg/container/docker_run.go
@@ -352,6 +352,8 @@ func (cr *containerReference) copyDir(dstPath string, srcPath string) common.Exe
 
 			// update the name to correctly reflect the desired destination when untaring
 			header.Name = strings.TrimPrefix(file, srcPrefix)
+			header.Mode = int64(fi.Mode())
+			header.ModTime = fi.ModTime()
 
 			// write the header
 			if err := tw.WriteHeader(header); err != nil {
