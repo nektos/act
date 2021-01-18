34cce3d910cbd0f202839a9d0d2efbe419a8a613
hackercat <me@hackerc.at>
2021-01-18 00:27:23 +0000

    Add autodetect event flag

--- cmd/root.go
+++ cmd/root.go
@@ -42,6 +42,7 @@ func Execute(ctx context.Context, version string) {
 	rootCmd.Flags().BoolVarP(&input.reuseContainers, "reuse", "r", false, "reuse action containers to maintain state")
 	rootCmd.Flags().BoolVarP(&input.bindWorkdir, "bind", "b", false, "bind working directory to container, rather than copy")
 	rootCmd.Flags().BoolVarP(&input.forcePull, "pull", "p", false, "pull docker image(s) if already present")
+	rootCmd.Flags().BoolVarP(&input.autodetectEvent, "detect-event", "", false, "Use first event type from workflow as event that triggered the workflow")
 	rootCmd.Flags().StringVarP(&input.eventPath, "eventpath", "e", "", "path to event JSON file")
 	rootCmd.Flags().StringVar(&input.defaultBranch, "defaultbranch", "", "the name of the main branch")
 	rootCmd.Flags().BoolVar(&input.privileged, "privileged", false, "use privileged mode")
@@ -145,15 +146,18 @@ func newRunCommand(ctx context.Context, input *Input) func(*cobra.Command, []str
 
 		// Determine the event name
 		var eventName string
-		if len(args) > 0 {
-			eventName = args[0]
-		} else if plan := planner.PlanEvent("push"); plan != nil {
-			eventName = "push"
-		} else if events := planner.GetEvents(); len(events) > 0 {
-			// set default event type to first event
+		events := planner.GetEvents()
+		if input.autodetectEvent && len(events) > 0 {
+			 // set default event type to first event
 			// this way user dont have to specify the event.
 			log.Debugf("Using detected workflow event: %s", events[0])
 			eventName = events[0]
+		} else {
+			if len(args) > 0 {
+				eventName = args[0]
+			} else if plan := planner.PlanEvent("push"); plan != nil {
+				eventName = "push"
+			}
 		}
 
 		// build the plan for this run
