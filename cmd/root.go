c411062b6b22fc796b15518c92588aa731ea3eff
hackercat <me@hackerc.at>
2021-01-19 04:34:35 +0000

    Use image from DockerHub

--- cmd/root.go
+++ cmd/root.go
@@ -96,7 +96,7 @@ func args() []string {
 			case "Large":
 				option = "-P ubuntu-latest=nektos/act-environments-ubuntu:18.04\n-P ubuntu-18.04=nektos/act-environments-ubuntu:18.04"
 			case "Medium":
-				option = "-P ubuntu-latest=ghcr.io/catthehacker/ubuntu:act-latest\n-P ubuntu-20.04=ghcr.io/catthehacker/ubuntu:act-20.04\n-P ubuntu-18.04=ghcr.io/catthehacker/ubuntu:act-18.04\nubuntu-16.04=ghcr.io/catthehacker/ubuntu:act-16.04"
+				option = "-P ubuntu-latest=catthehacker/ubuntu:act-latest\n-P ubuntu-20.04=catthehacker/ubuntu:act-20.04\n-P ubuntu-18.04=catthehacker/ubuntu:act-18.04\nubuntu-16.04=catthehacker/ubuntu:act-16.04"
 			case "Micro":
 				option = "-P ubuntu-latest=node:12.6-buster-slim\n-P ubuntu-12.04=node:12.6-buster-slim\n-P ubuntu-18.04=node:12.6-buster-slim\n-P ubuntu-16.04=node:12.6-stretch-slim"
 			}
