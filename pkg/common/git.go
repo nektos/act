9fd1fe2589793613b94670363e308cca1a46498b
Josh Soref <jsoref@users.noreply.github.com>
2021-03-01 21:21:01 -0500

    Comment for public function

--- pkg/common/git.go
+++ pkg/common/git.go
@@ -189,6 +189,7 @@ type NewGitCloneExecutorInput struct {
 	Dir string
 }
 
+// CloneIfRequired ...
 func CloneIfRequired(refName plumbing.ReferenceName, input NewGitCloneExecutorInput, logger log.FieldLogger) (*git.Repository, error) {
 	r, err := git.PlainOpen(input.Dir)
 	if err != nil {
