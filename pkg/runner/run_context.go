ff859877b976103371eff97575cf33bb69ca4f75
Torbjørn Vatn <torbjorn.vatn@unacast.com>
2021-03-29 17:03:26 +0200

    Fix typo

--- pkg/runner/run_context.go
+++ pkg/runner/run_context.go
@@ -586,7 +586,7 @@ func (rc *RunContext) withGithubEnv(env map[string]string) map[string]string {
 	env["CI"] = "true"
 	env["HOME"] = fmt.Sprintf("%s/%s", filepath.Dir(rc.Config.Workdir), "home")
 	env["GITHUB_ENV"] = fmt.Sprintf("%s/%s", filepath.Dir(rc.Config.Workdir), "workflow/envs.txt")
-	env["GITHUB_ENV"] = fmt.Sprintf("%s/%s", filepath.Dir(rc.Config.Workdir), "workflow/paths.txt")
+	env["GITHUB_PATH"] = fmt.Sprintf("%s/%s", filepath.Dir(rc.Config.Workdir), "workflow/paths.txt")
 	env["GITHUB_WORKFLOW"] = github.Workflow
 	env["GITHUB_RUN_ID"] = github.RunID
 	env["GITHUB_RUN_NUMBER"] = github.RunNumber
