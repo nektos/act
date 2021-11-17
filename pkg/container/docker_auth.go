1a9bb7d36207764aae968780dfb118932bc81e0b
Björn Brauer <bjoern.brauer@new-work.se>
2021-11-17 22:10:09 +0000

    feat: read docker credentials from local docker config

---
+++ pkg/container/docker_auth.go
@@ -0,0 +1,41 @@
+package container
+
+import (
+	"net/url"
+	"regexp"
+
+	"github.com/docker/cli/cli/config"
+	"github.com/docker/cli/cli/config/credentials"
+	"github.com/docker/docker/api/types"
+	log "github.com/sirupsen/logrus"
+)
+
+func LoadDockerAuthConfig(image string) (types.AuthConfig, error) {
+	config, err := config.Load(config.Dir())
+	if err != nil {
+		log.Warnf("Could not load docker config: %v", err)
+		return types.AuthConfig{}, err
+	}
+
+	if !config.ContainsAuth() {
+		config.CredentialsStore = credentials.DetectDefaultStore(config.CredentialsStore)
+	}
+
+	if matches, _ := regexp.MatchString("^[^.]+\\/", image); matches {
+		image = "https://index.docker.io/v1/" + image
+	}
+
+	parsed, err := url.Parse(image)
+	if err != nil {
+		log.Warnf("Could not parse image url: %v", err)
+		return types.AuthConfig{}, err
+	}
+
+	authConfig, err := config.GetAuthConfig(parsed.Hostname())
+	if err != nil {
+		log.Warnf("Could not get auth config from docker config: %v", err)
+		return types.AuthConfig{}, err
+	}
+
+	return types.AuthConfig(authConfig), nil
+}
