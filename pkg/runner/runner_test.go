3a2f413d556976b4a44385a79bd0f14f85984dbb
Ryan <me@hackerc.at>
2022-03-30 18:17:15 +0200

    fix: return error on reusable workflows

--- pkg/runner/runner_test.go
+++ pkg/runner/runner_test.go
@@ -123,6 +123,7 @@ func TestRunEvent(t *testing.T) {
 		{"testdata", "uses-composite", "push", "", platforms, ""},
 		{"testdata", "uses-composite-with-error", "push", "Job 'failing-composite-action' failed", platforms, ""},
 		{"testdata", "uses-nested-composite", "push", "", platforms, ""},
+		{"testdata", "uses-workflow", "push", "reusable workflows are currently not supported (see https://github.com/nektos/act/issues/826 for updates)", platforms, ""},
 		{"testdata", "composite-fail-with-output", "push", "", platforms, ""},
 		{"testdata", "issue-597", "push", "", platforms, ""},
 		{"testdata", "issue-598", "push", "", platforms, ""},
