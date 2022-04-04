92ba0d22114d3a2761747a84c1b0106f7889ed6d
Markus Wolf <mail@markus-wolf.de>
2022-04-04 11:51:59 +0200

    refactor: remove unused lines of code

--- pkg/runner/testdata/uses-composite-with-pre-and-post-steps/action-with-pre-and-post/post.js
+++ pkg/runner/testdata/uses-composite-with-pre-and-post-steps/action-with-pre-and-post/post.js
@@ -1,4 +1,3 @@
 const { appendFileSync } = require('fs');
-const env = process.env['STEP_OUTPUT_TEST'];
 const step = process.env['INPUT_STEP'];
 appendFileSync(process.env['GITHUB_ENV'], `;${step}-post`, { encoding:'utf-8' })
