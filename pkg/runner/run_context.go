b9828deb3aa099983a83b37adda8c235743d4f7b
Christopher Homberger <christopher.homberger@web.de>
2022-08-05 20:26:34 +0200

    stability fixes

--- pkg/runner/run_context.go
+++ pkg/runner/run_context.go
@@ -65,8 +65,11 @@ func (rc *RunContext) String() string {
 
 // GetEnv returns the env for the context
 func (rc *RunContext) GetEnv() map[string]string {
-	if rc.Env == nil {
-		rc.Env = mergeMaps(rc.Run.Workflow.Env, rc.Run.Job().Environment(), rc.Config.Env)
+	if rc.Env == nil && rc.Run != nil && rc.Run.Workflow != nil && rc.Config != nil {
+		job := rc.Run.Job()
+		if job != nil && rc.Config != nil {
+			rc.Env = mergeMaps(rc.Run.Workflow.Env, job.Environment(), rc.Config.Env)
+		}
 	}
 	rc.Env["ACT"] = "true"
 	return rc.Env
