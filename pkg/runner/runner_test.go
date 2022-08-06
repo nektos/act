b6af3675adb09a0b69e796ed263438b7c7979cd8
Christopher Homberger <christopher.homberger@web.de>
2022-08-06 22:35:17 +0200

    Add initial Tests

--- pkg/runner/runner_test.go
+++ pkg/runner/runner_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"testing"
 
@@ -181,6 +182,102 @@ func TestRunEvent(t *testing.T) {
 		{"../model/testdata", "strategy", "push", "", platforms}, // TODO: move all testdata into pkg so we can validate it with planner and runner
 		// {"testdata", "issue-228", "push", "", platforms, }, // TODO [igni]: Remove this once everything passes
 		{"../model/testdata", "container-volumes", "push", "", platforms},
+	}
+
+	for _, table := range tables {
+		t.Run(table.workflowPath, func(t *testing.T) {
+			table.runTest(ctx, t, &Config{})
+		})
+	}
+}
+
+func TestRunEventHostEnvironment(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	ctx := context.Background()
+
+	tables := []TestJobFileInfo{}
+
+	if runtime.GOOS == "linux" {
+		platforms := map[string]string{
+			"ubuntu-latest": "-self-hosted",
+		}
+
+		tables = append(tables, []TestJobFileInfo{
+			// Shells
+			{workdir, "shells/defaults", "push", "", platforms},
+			{workdir, "shells/pwsh", "push", "", platforms},
+			{workdir, "shells/bash", "push", "", platforms},
+			{workdir, "shells/python", "push", "", platforms},
+			{workdir, "shells/sh", "push", "", platforms},
+
+			// Local action
+			{workdir, "local-action-docker-url", "push", "", platforms},
+			{workdir, "local-action-dockerfile", "push", "", platforms},
+			{workdir, "local-action-via-composite-dockerfile", "push", "", platforms},
+			{workdir, "local-action-js", "push", "", platforms},
+
+			// Uses
+			{workdir, "uses-composite", "push", "", platforms},
+			{workdir, "uses-composite-with-error", "push", "Job 'failing-composite-action' failed", platforms},
+			{workdir, "uses-nested-composite", "push", "", platforms},
+			{workdir, "act-composite-env-test", "push", "", platforms},
+
+			// Eval
+			{workdir, "evalmatrix", "push", "", platforms},
+			{workdir, "evalmatrixneeds", "push", "", platforms},
+			{workdir, "evalmatrixneeds2", "push", "", platforms},
+			{workdir, "evalmatrix-merge-map", "push", "", platforms},
+			{workdir, "evalmatrix-merge-array", "push", "", platforms},
+			{workdir, "issue-1195", "push", "", platforms},
+
+			{workdir, "basic", "push", "", platforms},
+			{workdir, "fail", "push", "exit with `FAILURE`: 1", platforms},
+			{workdir, "runs-on", "push", "", platforms},
+			{workdir, "checkout", "push", "", platforms},
+			{workdir, "remote-action-docker", "push", "", platforms},
+			{workdir, "remote-action-js", "push", "", platforms},
+			{workdir, "matrix", "push", "", platforms},
+			{workdir, "matrix-include-exclude", "push", "", platforms},
+			{workdir, "commands", "push", "", platforms},
+			{workdir, "workdir", "push", "", platforms},
+			{workdir, "defaults-run", "push", "", platforms},
+			{workdir, "composite-fail-with-output", "push", "", platforms},
+			{workdir, "issue-597", "push", "", platforms},
+			{workdir, "issue-598", "push", "", platforms},
+			{workdir, "if-env-act", "push", "", platforms},
+			{workdir, "env-and-path", "push", "", platforms},
+			{workdir, "non-existent-action", "push", "Job 'nopanic' failed", platforms},
+			{workdir, "outputs", "push", "", platforms},
+			{workdir, "steps-context/conclusion", "push", "", platforms},
+			{workdir, "steps-context/outcome", "push", "", platforms},
+			{workdir, "job-status-check", "push", "job 'fail' failed", platforms},
+			{workdir, "if-expressions", "push", "Job 'mytest' failed", platforms},
+			{workdir, "actions-environment-and-context-tests", "push", "", platforms},
+			{workdir, "uses-action-with-pre-and-post-step", "push", "", platforms},
+			{workdir, "evalenv", "push", "", platforms},
+			{workdir, "ensure-post-steps", "push", "Job 'second-post-step-should-fail' failed", platforms},
+		}...)
+	}
+	if runtime.GOOS == "windows" {
+		platforms := map[string]string{
+			"windows-latest": "-self-hosted",
+		}
+
+		tables = append(tables, []TestJobFileInfo{
+			{workdir, "windows-prepend-path", "push", "", platforms},
+			{workdir, "windows-add-env", "push", "", platforms},
+		}...)
+	} else {
+		platforms := map[string]string{
+			"self-hosted": "-self-hosted",
+		}
+
+		tables = append(tables, []TestJobFileInfo{
+			{workdir, "nix-prepend-path", "push", "", platforms},
+		}...)
 	}
 
 	for _, table := range tables {
