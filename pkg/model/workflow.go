d1daf2f28d8c2256e5712e3064ce6ac45fca2562
Alex Savchuk <a.u.savchuk@gmail.com>
2022-09-08 17:20:39 +0300

    fix: support expression for step's continue-on-error field (#900) (#1331)
    Co-authored-by: Markus Wolf <KnisterPeter@users.noreply.github.com>

--- pkg/model/workflow.go
+++ pkg/model/workflow.go
@@ -350,17 +350,17 @@ type ContainerSpec struct {
 
 // Step is the structure of one step in a job
 type Step struct {
-	ID               string            `yaml:"id"`
-	If               yaml.Node         `yaml:"if"`
-	Name             string            `yaml:"name"`
-	Uses             string            `yaml:"uses"`
-	Run              string            `yaml:"run"`
-	WorkingDirectory string            `yaml:"working-directory"`
-	Shell            string            `yaml:"shell"`
-	Env              yaml.Node         `yaml:"env"`
-	With             map[string]string `yaml:"with"`
-	ContinueOnError  bool              `yaml:"continue-on-error"`
-	TimeoutMinutes   string            `yaml:"timeout-minutes"`
+	ID                 string            `yaml:"id"`
+	If                 yaml.Node         `yaml:"if"`
+	Name               string            `yaml:"name"`
+	Uses               string            `yaml:"uses"`
+	Run                string            `yaml:"run"`
+	WorkingDirectory   string            `yaml:"working-directory"`
+	Shell              string            `yaml:"shell"`
+	Env                yaml.Node         `yaml:"env"`
+	With               map[string]string `yaml:"with"`
+	RawContinueOnError string            `yaml:"continue-on-error"`
+	TimeoutMinutes     string            `yaml:"timeout-minutes"`
 }
 
 // String gets the name of step
