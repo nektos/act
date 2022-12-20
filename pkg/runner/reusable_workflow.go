2627c955d0466b812ecd19b3bd05972817d97bdb
Markus Wolf <mail@markus-wolf.de>
2022-12-20 14:22:37 +0100

    feat: add remote reusable workflows
    This changes adds cloning of a remote repository to
    run a workflow included in it.
    
    Closes #826

--- pkg/runner/reusable_workflow.go
+++ pkg/runner/reusable_workflow.go
@@ -3,21 +3,46 @@ package runner
 import (
 	"fmt"
 	"path"
+	"regexp"
+	"strings"
 
 	"github.com/nektos/act/pkg/common"
+	"github.com/nektos/act/pkg/common/git"
 	"github.com/nektos/act/pkg/model"
 )
 
 func newLocalReusableWorkflowExecutor(rc *RunContext) common.Executor {
-	return newReusableWorkflowExecutor(rc, rc.Config.Workdir)
+	return newReusableWorkflowExecutor(rc, rc.Config.Workdir, rc.Run.Job().Uses)
 }
 
 func newRemoteReusableWorkflowExecutor(rc *RunContext) common.Executor {
-	return common.NewErrorExecutor(fmt.Errorf("remote reusable workflows are currently not supported (see https://github.com/nektos/act/issues/826 for updates)"))
+	uses := rc.Run.Job().Uses
+
+	remoteReusableWorkflow := newRemoteReusableWorkflow(uses)
+	if remoteReusableWorkflow == nil {
+		return common.NewErrorExecutor(fmt.Errorf("expected format {owner}/{repo}/.github/workflows/{filename}@{ref}. Actual '%s' Input string was not in a correct format", uses))
+	}
+
+	remoteReusableWorkflow.URL = rc.Config.GitHubInstance
+
+	// todo: really use rc.ActionCacheDir()?
+	workflowDir := fmt.Sprintf("%s/%s", rc.ActionCacheDir(), strings.ReplaceAll(uses, "/", "-"))
+
+	gitClone := git.NewGitCloneExecutor(git.NewGitCloneExecutorInput{
+		URL:   remoteReusableWorkflow.CloneURL(),
+		Ref:   remoteReusableWorkflow.Ref,
+		Dir:   workflowDir,
+		Token: rc.Config.Token,
+	})
+
+	return common.NewPipelineExecutor(
+		gitClone,
+		newReusableWorkflowExecutor(rc, workflowDir, fmt.Sprintf("./.github/workflows/%s", remoteReusableWorkflow.Filename)),
+	)
 }
 
-func newReusableWorkflowExecutor(rc *RunContext, directory string) common.Executor {
-	planner, err := model.NewWorkflowPlanner(path.Join(directory, rc.Run.Job().Uses), true)
+func newReusableWorkflowExecutor(rc *RunContext, directory string, workflow string) common.Executor {
+	planner, err := model.NewWorkflowPlanner(path.Join(directory, workflow), true)
 	if err != nil {
 		return common.NewErrorExecutor(err)
 	}
@@ -42,4 +67,33 @@ func NewReusableWorkflowRunner(rc *RunContext) (Runner, error) {
 	}
 
 	return runner.configure()
+}
+
+type remoteReusableWorkflow struct {
+	URL      string
+	Org      string
+	Repo     string
+	Filename string
+	Ref      string
+}
+
+func (r *remoteReusableWorkflow) CloneURL() string {
+	return fmt.Sprintf("https://%s/%s/%s", r.URL, r.Org, r.Repo)
+}
+
+func newRemoteReusableWorkflow(uses string) *remoteReusableWorkflow {
+	// GitHub docs:
+	// https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_iduses
+	r := regexp.MustCompile(`^([^/]+)/([^/]+)/.github/workflows/([^@]+)@(.*)$`)
+	matches := r.FindStringSubmatch(uses)
+	if len(matches) != 5 {
+		return nil
+	}
+	return &remoteReusableWorkflow{
+		Org:      matches[1],
+		Repo:     matches[2],
+		Filename: matches[3],
+		Ref:      matches[4],
+		URL:      "github.com",
+	}
 }
