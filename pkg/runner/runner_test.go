7323d7c673a910c025478770add3ec2a8e7b1093
Markus Wolf <mail@markus-wolf.de>
2022-12-20 18:01:38 +0100

    test: define test case of path issues
    Test case for #1528

--- pkg/runner/runner_test.go
+++ pkg/runner/runner_test.go
@@ -200,6 +200,7 @@ func TestRunEvent(t *testing.T) {
 		{"../model/testdata", "strategy", "push", "", platforms, secrets}, // TODO: move all testdata into pkg so we can validate it with planner and runner
 		// {"testdata", "issue-228", "push", "", platforms, }, // TODO [igni]: Remove this once everything passes
 		{"../model/testdata", "container-volumes", "push", "", platforms, secrets},
+		{workdir, "path-handling", "push", "", platforms, secrets},
 	}
 
 	for _, table := range tables {
