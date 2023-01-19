566b9d843e3da42736f6a9d51b6f95050cd65baa
Yoshiaki Yoshida <y.yoshida22@gmail.com>
2023-01-19 15:49:16 +0900

    Fixed auto-generated platform configuration with Micro size image (#1566)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- cmd/root.go
+++ cmd/root.go
@@ -533,7 +533,7 @@ func defaultImageSurvey(actrc string) error {
 	case "Medium":
 		option = "-P ubuntu-latest=catthehacker/ubuntu:act-latest\n-P ubuntu-22.04=catthehacker/ubuntu:act-22.04\n-P ubuntu-20.04=catthehacker/ubuntu:act-20.04\n-P ubuntu-18.04=catthehacker/ubuntu:act-18.04\n"
 	case "Micro":
-		option = "-P ubuntu-latest=node:16-buster-slim\n-P -P ubuntu-22.04=node:16-bullseye-slim\n ubuntu-20.04=node:16-buster-slim\n-P ubuntu-18.04=node:16-buster-slim\n"
+		option = "-P ubuntu-latest=node:16-buster-slim\n-P ubuntu-22.04=node:16-bullseye-slim\n-P ubuntu-20.04=node:16-buster-slim\n-P ubuntu-18.04=node:16-buster-slim\n"
 	}
 
 	f, err := os.Create(actrc)
