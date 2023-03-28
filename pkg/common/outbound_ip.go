9ab71eb01fce4d2b55e64651e7177307d6f4d84a
Jason Song <i@wolfogre.com>
2023-03-28 15:45:43 +0800

    Revert "fix: common"
    This reverts commit 6599803b6ae3b7adc168ef41b4afd4d89fc22f34.

--- pkg/common/outbound_ip.go
+++ pkg/common/outbound_ip.go
@@ -2,18 +2,20 @@ package common
 
 import (
 	"net"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // https://stackoverflow.com/a/37382208
 // Get preferred outbound ip of this machine
-func GetOutboundIP() (net.IP, error) {
+func GetOutboundIP() net.IP {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
-		return nil, err
+		log.Fatal(err)
 	}
 	defer conn.Close()
 
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 
-	return localAddr.IP, nil
+	return localAddr.IP
 }
