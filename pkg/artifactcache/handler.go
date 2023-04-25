ae20b92a493b55d9046a10c91055dce5a49ab6e4
Jason Song <i@wolfogre.com>
2023-04-25 21:08:54 +0800

    chore: one less file

--- pkg/artifactcache/handler.go
+++ pkg/artifactcache/handler.go
@@ -423,3 +423,19 @@ func (h *Handler) responseJson(w http.ResponseWriter, r *http.Request, code int,
 	w.WriteHeader(code)
 	_, _ = w.Write(data)
 }
+
+func parseContentRange(s string) (int64, int64, error) {
+	// support the format like "bytes 11-22/*" only
+	s, _, _ = strings.Cut(strings.TrimPrefix(s, "bytes "), "/")
+	s1, s2, _ := strings.Cut(s, "-")
+
+	start, err := strconv.ParseInt(s1, 10, 64)
+	if err != nil {
+		return 0, 0, fmt.Errorf("parse %q: %w", s, err)
+	}
+	stop, err := strconv.ParseInt(s2, 10, 64)
+	if err != nil {
+		return 0, 0, fmt.Errorf("parse %q: %w", s, err)
+	}
+	return start, stop, nil
+}
