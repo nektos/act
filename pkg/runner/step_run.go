83140951bf96c500f0c1a745e4333b27f2820e49
ChristopherHX <christopher.homberger@web.de>
2023-08-08 17:44:25 +0200

    feat: cmd support for windows (#1941)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pkg/runner/step_run.go
+++ pkg/runner/step_run.go
@@ -18,6 +18,7 @@ type stepRun struct {
 	Step             *model.Step
 	RunContext       *RunContext
 	cmd              []string
+	cmdline          string
 	env              map[string]string
 	WorkingDirectory string
 }
@@ -34,6 +35,9 @@ func (sr *stepRun) main() common.Executor {
 		sr.setupShellCommandExecutor(),
 		func(ctx context.Context) error {
 			sr.getRunContext().ApplyExtraPath(ctx, &sr.env)
+			if he, ok := sr.getRunContext().JobContainer.(*container.HostEnvironment); ok && he != nil {
+				return he.ExecWithCmdLine(sr.cmd, sr.cmdline, sr.env, "", sr.WorkingDirectory)(ctx)
+			}
 			return sr.getRunContext().JobContainer.Exec(sr.cmd, sr.env, "", sr.WorkingDirectory)(ctx)
 		},
 	))
@@ -135,7 +139,8 @@ func (sr *stepRun) setupShellCommand(ctx context.Context) (name, script string, 
 
 	rc := sr.getRunContext()
 	scriptPath := fmt.Sprintf("%s/%s", rc.JobContainer.GetActPath(), name)
-	sr.cmd, err = shellquote.Split(strings.Replace(scCmd, `{0}`, scriptPath, 1))
+	sr.cmdline = strings.Replace(scCmd, `{0}`, scriptPath, 1)
+	sr.cmd, err = shellquote.Split(sr.cmdline)
 
 	return name, script, err
 }
