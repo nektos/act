18b4714e38d4871947eff0acb7f1c8f4625fda9f
Jon Jensen <jenseng@gmail.com>
2023-11-12 12:52:08 -0500

    Don't set GITHUB_TOKEN (#2089)
    This needs to be explicitly in the `env` to be consistent with GitHub
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pkg/runner/run_context.go
+++ pkg/runner/run_context.go
@@ -911,7 +911,6 @@ func (rc *RunContext) withGithubEnv(ctx context.Context, github *model.GithubCon
 	env["GITHUB_REF"] = github.Ref
 	env["GITHUB_REF_NAME"] = github.RefName
 	env["GITHUB_REF_TYPE"] = github.RefType
-	env["GITHUB_TOKEN"] = github.Token
 	env["GITHUB_JOB"] = github.Job
 	env["GITHUB_REPOSITORY_OWNER"] = github.RepositoryOwner
 	env["GITHUB_RETENTION_DAYS"] = github.RetentionDays
