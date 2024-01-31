a461c629129619bc068f76426e9a753181166f8e
Kristofer <kristofer@andes.is>
2024-01-31 16:05:47 +0000

    Write tests to verify functionality

--- pkg/container/util_test.go
+++ pkg/container/util_test.go
@@ -1,7 +1,6 @@
 package container
 
 import (
-	"context"
 	"testing"
 
 	log "github.com/sirupsen/logrus"
@@ -12,27 +11,26 @@ func init() {
 	log.SetLevel(log.DebugLevel)
 }
 
-func TestGetSocketAndHost(t *testing.T) {
-	// Assert tuple
-	ret, err := GetSocketAndHost("-", "DOCKER_HOST")
+func TestGetSocketAndHostWithSocket(t *testing.T) {
+	ret, err := GetSocketAndHost("/path/to/my.socket", "DOCKER_HOST")
+	assert.NotErrorIs(t, err, nil)
+	assert.Equal(t, SocketAndHost{"/path/to/my.socket", "/var/run/docker.sock"}, ret)
+}
+
+func TestGetSocketAndHostNoSocket(t *testing.T) {
+	ret, err := GetSocketAndHost("", "DOCKER_HOST")
+	assert.NotErrorIs(t, err, nil)
+	assert.Equal(t, SocketAndHost{"/var/run/docker.sock", "/var/run/docker.sock"}, ret)
+}
+
+func TestGetSocketAndHostOnlySocket(t *testing.T) {
+	ret, err := GetSocketAndHost("/path/to/my.socket", "")
+	assert.NotErrorIs(t, err, nil)
+	assert.Equal(t, SocketAndHost{"/path/to/my.socket", "/path/to/my.socket"}, ret)
+}
+
+func TestGetSocketAndHostDontMount(t *testing.T) {
+	ret, err := GetSocketAndHost("-", "DOKCER_HOST")
 	assert.NotErrorIs(t, err, nil)
 	assert.Equal(t, SocketAndHost{"-", "/var/run/docker.sock"}, ret)
-
-	tables := []struct {
-		imageIn  string
-		imageOut string
-	}{
-		{"myhost.com/foo/bar", "myhost.com/foo/bar"},
-		{"localhost:8000/canonical/ubuntu", "localhost:8000/canonical/ubuntu"},
-		{"localhost/canonical/ubuntu:latest", "localhost/canonical/ubuntu:latest"},
-		{"localhost:8000/canonical/ubuntu:latest", "localhost:8000/canonical/ubuntu:latest"},
-		{"ubuntu", "docker.io/library/ubuntu"},
-		{"ubuntu:18.04", "docker.io/library/ubuntu:18.04"},
-		{"cibuilds/hugo:0.53", "docker.io/cibuilds/hugo:0.53"},
-	}
-
-	for _, table := range tables {
-		imageOut := cleanImage(context.Background(), table.imageIn)
-		assert.Equal(t, table.imageOut, imageOut)
-	}
 }
