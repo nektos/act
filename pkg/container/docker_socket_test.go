1e8910e6c0b4845b34fbd84565191d8c498553f1
Kristofer <kristofer@andes.is>
2024-01-31 18:49:53 +0000

    Rename corresponding test

--- pkg/container/docker_socket_test.go
+++ pkg/container/docker_socket_test.go
@@ -1,6 +1,7 @@
 package container
 
 import (
+	"os"
 	"testing"
 
 	log "github.com/sirupsen/logrus"
@@ -12,25 +13,36 @@ func init() {
 }
 
 func TestGetSocketAndHostWithSocket(t *testing.T) {
+	os.Setenv("DOCKER_HOST", "unix:///var/run/docker.sock")
 	ret, err := GetSocketAndHost("/path/to/my.socket")
 	assert.NotErrorIs(t, err, nil)
 	assert.Equal(t, SocketAndHost{"/path/to/my.socket", "/var/run/docker.sock"}, ret)
 }
 
 func TestGetSocketAndHostNoSocket(t *testing.T) {
+	os.Setenv("DOCKER_HOST", "unix:///var/run/docker.sock")
 	ret, err := GetSocketAndHost("")
 	assert.NotErrorIs(t, err, nil)
-	assert.Equal(t, SocketAndHost{"/var/run/docker.sock", "/var/run/docker.sock"}, ret)
+	assert.Equal(t, SocketAndHost{"/var/run/docker.sock", "unix:///var/run/docker.sock"}, ret)
 }
 
 func TestGetSocketAndHostOnlySocket(t *testing.T) {
+	os.Setenv("DOCKER_HOST", "unix:///var/run/docker.sock")
 	ret, err := GetSocketAndHost("/path/to/my.socket")
 	assert.NotErrorIs(t, err, nil)
-	assert.Equal(t, SocketAndHost{"/path/to/my.socket", "/path/to/my.socket"}, ret)
+	assert.Equal(t, SocketAndHost{"/path/to/my.socket", "unix:///path/to/my.socket"}, ret)
 }
 
 func TestGetSocketAndHostDontMount(t *testing.T) {
+	os.Setenv("DOCKER_HOST", "unix:///var/run/docker.sock")
 	ret, err := GetSocketAndHost("-")
 	assert.NotErrorIs(t, err, nil)
-	assert.Equal(t, SocketAndHost{"-", "/var/run/docker.sock"}, ret)
+	assert.Equal(t, SocketAndHost{"-", "unix:///var/run/docker.sock"}, ret)
+}
+
+func TestGetSocketAndHost(t *testing.T) {
+	os.Setenv("DOCKER_HOST", "unix:///var/run/docker.sock")
+	ret, err := GetSocketAndHost("-")
+	assert.NotErrorIs(t, err, nil)
+	assert.Equal(t, SocketAndHost{"-", "unix:///var/run/docker.sock"}, ret)
 }
