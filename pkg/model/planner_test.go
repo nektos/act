9d7cdd8420f7629e7ad6970cd8fab804b668aa5e
Andreas Taylor <Andy4495@users.noreply.github.com>
2024-04-17 21:47:16 -0500

    Remove unnecessary checks

--- pkg/model/planner_test.go
+++ pkg/model/planner_test.go
@@ -51,11 +51,6 @@ func TestWorkflow(t *testing.T) {
 		},
 	}
 
-	// Check that an invalid job id returns error
-	result, err := createStages(&workflow, "invalid_job_id")
-	assert.NotNil(t, err)
-	assert.Nil(t, result)
-
 	// Check that an valid job id returns non-error
 	result, err = createStages(&workflow, "valid_job")
 	assert.Nil(t, err)
