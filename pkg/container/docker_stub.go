3c7eda7f3ea9ce6fe7358a802b19474b9d207447
ChristopherHX <christopher.homberger@web.de>
2024-07-10 18:45:07 +0200

    fix: docker stub and add a test for this (#2355)
    * fix: docker stub
    
    * test if you can build run without docker
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- pkg/container/docker_stub.go
+++ pkg/container/docker_stub.go
@@ -6,7 +6,7 @@ import (
 	"context"
 	"runtime"
 
-	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/system"
 	"github.com/nektos/act/pkg/common"
 	"github.com/pkg/errors"
 )
@@ -46,8 +46,8 @@ func RunnerArch(ctx context.Context) string {
 	return runtime.GOOS
 }
 
-func GetHostInfo(ctx context.Context) (info types.Info, err error) {
-	return types.Info{}, nil
+func GetHostInfo(ctx context.Context) (info system.Info, err error) {
+	return system.Info{}, nil
 }
 
 func NewDockerVolumeRemoveExecutor(volume string, force bool) common.Executor {
