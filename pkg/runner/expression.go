7ce71d129baa4dd8f4d999410365ed1febbc1a1e
S. M. Mahmudul Haque <yamin@sap.com>
2024-10-25 10:31:47 +0200

    Revert "Remove redundant check"
    This reverts commit 63455960ec714eea7631a586bcd59bed449739fc.

--- pkg/runner/expression.go
+++ pkg/runner/expression.go
@@ -480,6 +480,7 @@ func rewriteSubExpression(ctx context.Context, in string, forceFormat bool) (str
 	return out, nil
 }
 
+//nolint:gocyclo
 func getEvaluatorInputs(ctx context.Context, rc *RunContext, step step, ghc *model.GithubContext) map[string]interface{} {
 	inputs := map[string]interface{}{}
 
@@ -515,6 +516,22 @@ func getEvaluatorInputs(ctx context.Context, rc *RunContext, step step, ghc *mod
 		}
 	}
 
+	if ghc.EventName == "workflow_call" {
+		config := rc.Run.Workflow.WorkflowCallConfig()
+		if config != nil && config.Inputs != nil {
+			for k, v := range config.Inputs {
+				value := nestedMapLookup(ghc.Event, "inputs", k)
+				if value == nil {
+					v.Default.Decode(&value)
+				}
+				if v.Type == "boolean" {
+					inputs[k] = value == "true"
+				} else {
+					inputs[k] = value
+				}
+			}
+		}
+	}
 	return inputs
 }
 
