ae166be1ec7955fde2639dcac4cc861667049f49
Ryan Fleet <rmfleet@gmail.com>
2025-05-23 22:03:41 -0400

    added info log when container image platform mismatched

--- pkg/container/docker_images.go
+++ pkg/container/docker_images.go
@@ -8,7 +8,22 @@ import (
 
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
+	"github.com/nektos/act/pkg/common"
 )
+
+// ImageArchitectureMatches checks if the image architecture matches the requested platform
+// and returns a boolean indicating if it does or not
+func ImageArchitectureMatches(ctx context.Context, inspectImage image.InspectResponse, platform string) bool {
+	imagePlatform := fmt.Sprintf("%s/%s", inspectImage.Os, inspectImage.Architecture)
+
+	if imagePlatform == platform {
+		return true
+	}
+
+	logger := common.Logger(ctx)
+	logger.Infof("image found but platform does not match: %s (image) != %s (platform)\n", imagePlatform, platform)
+	return false
+}
 
 // ImageExistsLocally returns a boolean indicating if an image with the
 // requested name, tag and architecture exists in the local docker image store
@@ -26,11 +41,11 @@ func ImageExistsLocally(ctx context.Context, imageName string, platform string) 
 		return false, err
 	}
 
-	if platform == "" || platform == "any" || fmt.Sprintf("%s/%s", inspectImage.Os, inspectImage.Architecture) == platform {
+	if platform == "" || platform == "any" {
 		return true, nil
 	}
 
-	return false, nil
+	return ImageArchitectureMatches(ctx, inspectImage, platform), nil
 }
 
 // RemoveImage removes image from local store, the function is used to run different
